bf8850c47d85dc24b8424db15ec101c30ab417ec
yehoon <jangyh0420@gmail.com>
2023-10-23 14:08:19 +0900

    Edit indentation

--- prompt_selection.py
+++ prompt_selection.py
@@ -64,47 +64,47 @@ def select_prompt_using_label(opt, label):
     prompts = {
         "qa": """You're a helpful AI assistant. Choose the correct answer for the question ### User or the appropriate answer that completes the ### User's sentence. You can only choose the option in the 'Options' list. If you are unsure of the correct answer, simply answer -1. You have to answer only the character or number. Below <example> is an example.
 
-    <example>
-    ### User
-    George wants to warm his hands quickly by rubbing them. Which skin surface will produce the most heat?
+<example>
+### User
+George wants to warm his hands quickly by rubbing them. Which skin surface will produce the most heat?
 
-    Options: ["A. wet palms", "B. dry palms", "C. palms covered with oil", "D. palms covered with lotion"]
+Options: ["A. wet palms", "B. dry palms", "C. palms covered with oil", "D. palms covered with lotion"]
 
-    ### Assistant
-    B
+### Assistant
+B
 
-    """,
-        "sc": """You're a helpful AI assistant. Choose the correct answer for the question ### User or the appropriate answer that completes the ### User's sentence. You can only choose the option in the 'Options' list. If you are unsure of the correct answer, simply answer -1. You have to answer only the character or number. Below <example> is an example.
+""",
+    "sc": """You're a helpful AI assistant. Choose the correct answer for the question ### User or the appropriate answer that completes the ### User's sentence. You can only choose the option in the 'Options' list. If you are unsure of the correct answer, simply answer -1. You have to answer only the character or number. Below <example> is an example.
 
-    <example>
-    ### User
-    Then, the man writes over the snow covering the window of a car, and a woman wearing winter clothes smiles. then
+<example>
+### User
+Then, the man writes over the snow covering the window of a car, and a woman wearing winter clothes smiles. then
 
-    Options: ["0. , the man adds wax to the windshield and cuts it.", "1. , a person board a ski lift, while two men supporting the head of the person wearing winter clothes snow as the we girls sled.", "2. , the man puts on a christmas coat, knitted with netting.", "3. , the man continues removing the snow on his car."]
+Options: ["0. , the man adds wax to the windshield and cuts it.", "1. , a person board a ski lift, while two men supporting the head of the person wearing winter clothes snow as the we girls sled.", "2. , the man puts on a christmas coat, knitted with netting.", "3. , the man continues removing the snow on his car."]
 
-    ### Assistant
-    3
+### Assistant
+3
 
-    """,
-        "general": """You're a helpful AI assistant. Choose the correct answer for the question ### User or the appropriate answer that completes the ### User's sentence. You can only choose the option in the 'Options' list. If you are unsure of the correct answer, simply answer -1. You have to answer only the character or number. Below <example 1> and <example 2> are examples.
+""",
+    "general": """You're a helpful AI assistant. Choose the correct answer for the question ### User or the appropriate answer that completes the ### User's sentence. You can only choose the option in the 'Options' list. If you are unsure of the correct answer, simply answer -1. You have to answer only the character or number. Below <example 1> and <example 2> are examples.
 
-    <example 1>
-    ### User
-    George wants to warm his hands quickly by rubbing them. Which skin surface will produce the most heat?
+<example 1>
+### User
+George wants to warm his hands quickly by rubbing them. Which skin surface will produce the most heat?
 
-    Options: ["A. wet palms", "B. dry palms", "C. palms covered with oil", "D. palms covered with lotion"]
+Options: ["A. wet palms", "B. dry palms", "C. palms covered with oil", "D. palms covered with lotion"]
 
-    ### Assistant
-    B
+### Assistant
+B
 
-    <example 2>
-    ### User
-    Then, the man writes over the snow covering the window of a car, and a woman wearing winter clothes smiles. then
+<example 2>
+### User
+Then, the man writes over the snow covering the window of a car, and a woman wearing winter clothes smiles. then
 
-    Options: ["0. , the man adds wax to the windshield and cuts it.", "1. , a person board a ski lift, while two men supporting the head of the person wearing winter clothes snow as the we girls sled.", "2. , the man puts on a christmas coat, knitted with netting.", "3. , the man continues removing the snow on his car."]
+Options: ["0. , the man adds wax to the windshield and cuts it.", "1. , a person board a ski lift, while two men supporting the head of the person wearing winter clothes snow as the we girls sled.", "2. , the man puts on a christmas coat, knitted with netting.", "3. , the man continues removing the snow on his car."]
 
-    ### Assistant
-    3
+### Assistant
+3
     """}
 
     if prompt_label.lower() == "qa":
