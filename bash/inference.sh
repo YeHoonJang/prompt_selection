c9eec8328d1918b6fafe518d4b6c93afc77a9b4f
yehoon <jangyh0420@gmail.com>
2023-10-23 13:56:00 +0900

    Edit

--- bash/inference.sh
+++ bash/inference.sh
@@ -3,4 +3,4 @@ python /home/yehoon/workspace/prompt_selection/inference.py --inference --model=
 
 
 # Prompt Select Version
-python /home/yehoon/workspace/prompt_selection/inference.py --inference --select_prompt --model="meta-llama/Llama-2-7b-hf" --dataset="Yehoon/arc_hella" --test_split="train" --prompt_style="upstage" --custom_prompt="/home/yehoon/workspace/prompt_selection/data/prompt/empty.txt" --output_dir="/home/yehoon/workspace/outputs/" --output_name="0907_ft_llama2_upstage_way" --generated_name="/home/yehoon/workspace/prompt_selection/outputs/...name!!"
+python /home/yehoon/workspace/prompt_selection/inference.py --inference --select_prompt --use_label --model="meta-llama/Llama-2-7b-hf" --dataset="Yehoon/arc_hella" --test_split="train" --prompt_style="upstage" --custom_prompt="/home/yehoon/workspace/prompt_selection/data/prompt/empty.txt" --output_dir="/home/yehoon/workspace/outputs/" --output_name="0907_ft_llama2_upstage_way" --generated_name="/home/yehoon/workspace/prompt_selection/outputs/selected_prompt.txt"
