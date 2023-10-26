a09502898c7294ecfec69ccb8b0a7c3fbda6efb4
Yehoon <jangyh0420@gmail.com>
2023-10-26 21:19:23 +0900

    Edit prompt

--- prompt_selection.py
+++ prompt_selection.py
@@ -64,9 +64,11 @@ B
 def select_prompt_using_label(opt, label):
     prompt_label = label
     prompts = {
-        "qa": """You're a helpful AI assistant. Choose the correct answer for the question ### User or the appropriate answer that completes the ### User's sentence. You can only choose the option in the 'Options' list. If you are unsure of the correct answer, simply answer -1. You have to answer only the character or number. Below <example> is an example.
+        "qa": """You're a helpful AI assistant. Choose the correct answer for the question ### User or the appropriate answer that completes the ### User's sentence. You can only choose the option in the 'Options' list. If you are unsure of the correct answer, simply answer -1. You have to answer only the character or number. Below <Task Description and example> is an example.
 
-<example>
+<Task Description and example>
+* qa: a task that answer the correct answer to a question.
+```
 ### User
 George wants to warm his hands quickly by rubbing them. Which skin surface will produce the most heat?
 
@@ -74,17 +76,21 @@ Options: ["A. wet palms", "B. dry palms", "C. palms covered with oil", "D. palms
 
 ### Assistant
 B
+```
 
 """,
     "sc": """### System
-You're a helpful AI assistant. Complete the 'Description' with an appropriate ending. You can only choose the option in the 'Options' list. Take a look at the <example> below and think carefully before answering question of ### User.
+You're a helpful AI assistant. Complete the 'Description' with an appropriate ending. You can only choose the option in the 'Options' list. Take a look at the <Task Description and example> below and think carefully before answering question of ### User.
 
-<example>
+<Task Description and example>
+* sc: a task that chooses the appropriate and logically following phrase to finish a given unfinished sentence including arithmetic inference.
+```
 Description: Then, the man writes over the snow covering the window of a car, and a woman wearing winter clothes smiles. the
 
 Options: [", the man adds wax to the windshield and cuts it.", ", a person board a ski lift, while two men supporting the head of the person wearing winter clothes snow as the we girls sled.", ", the man puts on a christmas coat, knitted with netting.", ", the man continues removing the snow on his car."]
 
 Answer: ", the man continues removing the snow on his car."
+```
 
 """,
     "general": """You're a helpful AI assistant. Choose the correct answer for the question ### User or the appropriate answer that completes the ### User's sentence. You can only choose the option in the 'Options' list. If you are unsure of the correct answer, simply answer -1. You have to answer only the character or number. Below <example 1> and <example 2> are examples.
