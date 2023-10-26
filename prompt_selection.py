58e7fdb99dc48b201c2c11377f6878b894808605
yehoon <jangyh0420@gmail.com>
2023-10-26 15:13:10 +0900

    Edit prompt

--- prompt_selection.py
+++ prompt_selection.py
@@ -17,21 +17,30 @@ Options: ["A. wet palms", "B. dry palms", "C. palms covered with oil", "D. palms
 B
 
 """,
-    "sc": """You're a helpful AI assistant. Choose the correct answer for the question ### User or the appropriate answer that completes the ### User's sentence. You can only choose the option in the 'Options' list. If you are unsure of the correct answer, simply answer -1. You have to answer only the character or number. Below <example> is an example.
+    "sc": """### System
+You're a helpful AI assistant. Complete the 'Description' with an appropriate ending. You can only choose the option in the 'Options' list. Take a look at the <example> below and think carefully before answering question of ### User.
 
 <example>
-### User
-Then, the man writes over the snow covering the window of a car, and a woman wearing winter clothes smiles. then
+Description: Then, the man writes over the snow covering the window of a car, and a woman wearing winter clothes smiles. the
 
-Options: ["0. , the man adds wax to the windshield and cuts it.", "1. , a person board a ski lift, while two men supporting the head of the person wearing winter clothes snow as the we girls sled.", "2. , the man puts on a christmas coat, knitted with netting.", "3. , the man continues removing the snow on his car."]
+Options: [", the man adds wax to the windshield and cuts it.", ", a person board a ski lift, while two men supporting the head of the person wearing winter clothes snow as the we girls sled.", ", the man puts on a christmas coat, knitted with netting.", ", the man continues removing the snow on his car."]
 
-### Assistant
-3
+Answer: ", the man continues removing the snow on his car."
 
 """,
     "general": """You're a helpful AI assistant. Choose the correct answer for the question ### User or the appropriate answer that completes the ### User's sentence. You can only choose the option in the 'Options' list. If you are unsure of the correct answer, simply answer -1. You have to answer only the character or number. Below <example 1> and <example 2> are examples.
 
 <example 1>
+### User
+Then, the man writes over the snow covering the window of a car, and a woman wearing winter clothes smiles. then
+
+Options: [", the man adds wax to the windshield and cuts it.", ", a person board a ski lift, while two men supporting the head of the person wearing winter clothes snow as the we girls sled.", ", the man puts on a christmas coat, knitted with netting.", ", the man continues removing the snow on his car."]
+
+### Assistant
+", the man continues removing the snow on his car."
+
+
+<example 2>
 ### User
 George wants to warm his hands quickly by rubbing them. Which skin surface will produce the most heat?
 
@@ -40,14 +49,6 @@ Options: ["A. wet palms", "B. dry palms", "C. palms covered with oil", "D. palms
 ### Assistant
 B
 
-<example 2>
-### User
-Then, the man writes over the snow covering the window of a car, and a woman wearing winter clothes smiles. then
-
-Options: ["0. , the man adds wax to the windshield and cuts it.", "1. , a person board a ski lift, while two men supporting the head of the person wearing winter clothes snow as the we girls sled.", "2. , the man puts on a christmas coat, knitted with netting.", "3. , the man continues removing the snow on his car."]
-
-### Assistant
-3
 """}
 
     if prompt_label.lower() == "qa":
