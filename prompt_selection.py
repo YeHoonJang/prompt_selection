34425535d2bd5eaaa80f7e73a6ca231e5061a487
yehoon <jangyh0420@gmail.com>
2023-12-08 16:34:43 +0900

    Update Sentiment Analysis prompt

--- prompt_selection.py
+++ prompt_selection.py
@@ -28,6 +28,20 @@ Options: [", the man adds wax to the windshield and cuts it.", ", a person board
 Answer: ", the man continues removing the snow on his car."
 
 """,
+    "sa": """### System
+You're a helpful AI assistant. Analyze the sentiment of ### User's text and choose the appropriate sentiment. You can only choose the sentiment option in the 'Options' list. Take a look at the <Task Description and example> below and think carefully before answering question of ### User.
+
+<Task Description and example>
+* sa: a task of determining the sentiment of an entire text, which is used to understand the meaning of the words expressed in the text.
+```
+its a totally average film with a few semi-alright action sequences that make the plot seem a little better and remind the viewer of the classic van dam films. parts of the plot don't make sense and seem to be added in to use up time. the end plot is that of a very basic type that doesn't leave the viewer guessing and any twists are obvious from the beginning. the end scene with the flask backs don't make sense as they are added in and seem to have little relevance to the history of van dam's character. not really worth watching again, bit disappointed in the end production, even though it is apparent it was shot on a low budget certain shots and sections in the film are of poor directed quality
+
+Options: ["0. Negative", "1. Positive", "2.Neutral"]
+
+Answer: 0. Negative
+```
+
+""",
     "general": """You're a helpful AI assistant. Choose the correct answer for the question ### User or the appropriate answer that completes the ### User's sentence. You can only choose the option in the 'Options' list. If you are unsure of the correct answer, simply answer -1. You have to answer only the character or number. Below <example 1> and <example 2> are examples.
 
 <example 1>
@@ -55,6 +69,8 @@ B
         prompt_blurb = prompts["qa"]
     elif prompt_label.lower() == "sc":
         prompt_blurb = prompts["sc"]
+    elif prompt_label.lower() == "sa":
+        prompt_blurb = prompts["sa"]
     else:
         prompt_blurb = prompts["general"]
 
@@ -94,14 +110,14 @@ Answer: ", the man continues removing the snow on his car."
 
 """,
     "sa": """### System
-You're a helpful AI assistant. Analyze the sentiment of the ### User text and choose the appropriate sentiment. You can only choose the option in the 'Options' list. Take a look at the <Task Description and example> below and think carefully before answering question of ### User.
+You're a helpful AI assistant. Analyze the sentiment of ### User's text and choose the appropriate sentiment. You can only choose the sentiment option in the 'Options' list. Take a look at the <Task Description and example> below and think carefully before answering question of ### User.
 
 <Task Description and example>
-* sa: a task of determining the emotional tone behind a series of words, used to gain an understanding of the attitudes, opinions and emotions expressed within a text.
+* sa: a task of determining the sentiment of an entire text, which is used to understand the meaning of the words expressed in the text.
 ```
 its a totally average film with a few semi-alright action sequences that make the plot seem a little better and remind the viewer of the classic van dam films. parts of the plot don't make sense and seem to be added in to use up time. the end plot is that of a very basic type that doesn't leave the viewer guessing and any twists are obvious from the beginning. the end scene with the flask backs don't make sense as they are added in and seem to have little relevance to the history of van dam's character. not really worth watching again, bit disappointed in the end production, even though it is apparent it was shot on a low budget certain shots and sections in the film are of poor directed quality
 
-Options: ["0. Negative", "1. Positive"]
+Options: ["0. Negative", "1. Positive", "2.Neutral"]
 
 Answer: 0. Negative
 ```
